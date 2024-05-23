c8e9d08b6d6edb2b1494f5cea5d9da7e30446ffb
Priyanka Baraiyya <priyankabaraiyya5@gmail.com>
2024-05-23 21:57:34 +0530

    Task Completed

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import { ThemeProvider } from './ThemeContext'; // Assuming ThemeContext provides theme context
+import useTheme from './useTheme';
+import Contact from './components/Contact';
+
+const AppContent = () => {
+  const { theme } = useTheme();
+
+  return (
+    <div className={theme}>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </div>
+  );
+};
+
+const App = () => {
+  return (
+    <Router>
+      <ThemeProvider>
+        <AppContent />
+      </ThemeProvider>
+    </Router>
+  );
+};
+
+export default App;
---
+++ src/components/Contact.jsx
@@ -0,0 +1,13 @@
+
+function Contact() {
+    return (
+      <div className="flex flex-col items-center justify-center h-[70vh]">
+        <h1 className="text-4xl font-bold mb-4">Contact Me</h1>
+        <p className="text-xl mb-2">Created by:Priyanka Baraiyya</p>
+        <p className="text-xl mb-2">Email Address: priyankabaraiyya5@gmail.com</p>
+        
+      </div>
+    );
+  }
+  
+  export default Contact;
